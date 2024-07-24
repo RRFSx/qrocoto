2d80952c5143dc87aaea01d7eeec08a075f1ef1c
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-07-24 14:25:43 -0600

    cleanup and bug fix

--- modulefiles/qrocoto.lua
+++ modulefiles/qrocoto.lua
@@ -1,16 +1,14 @@
 
-local version = "3.7.0"
-local base    = pathJoin("/apps/valgrind",version)
-
 local fn      = myFileName()
 local module  = myModuleFullName()
 local loc     = fn:find(module,1,true)-2
 local mdir    = fn:sub(1,loc)
-local pkg     = pathJoin(mdir, "/..")
+local pkg     = pathJoin(mdir, "/qrocoto")
 
---LmodMsgRaw("debug:"..fn..'\n')
---LmodMsgRaw("debug:"..loc..'\n')
---LmodMsgRaw("debug:"..mdir..'\n')
---LmodMsgRaw("debug:"..pkg..'\n')
+--LmodMsgRaw("debug-fn:"..fn..'\n')
+--LmodMsgRaw("debug-module:"..module..'\n')
+--LmodMsgRaw("debug-loc:"..loc..'\n')
+--LmodMsgRaw("debug-mdir:"..mdir..'\n')
+--LmodMsgRaw("debug-pkg:"..pkg..'\n')
 
 prepend_path("PATH",pkg)
